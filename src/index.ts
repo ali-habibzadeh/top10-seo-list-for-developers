75ae1799e9fce9bb2509c063e78dd74937a9e45a
Ali <ali@deepcrawl.com>
2019-10-17 20:37:48 +0100

    refactor

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import express from "express";
 
 import { apiRouter } from "./api/routes";
 
-const PORT = process.env.PORT || 4000;
-const app = express();
-app.use("/api", apiRouter).listen(PORT);
+express()
+  .use("/api", apiRouter)
+  .listen(process.env.PORT || 4000);
